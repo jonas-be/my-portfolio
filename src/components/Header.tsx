5734e37965c5dbcef7cf1177901ba19c287c38da
Jonas <90411737+jonas-be@users.noreply.github.com>
2022-11-20 20:16:58 +0100

    Update header Link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Options from './Options'
+import Link from "next/link";
 
 function Header() {
     return (
@@ -9,12 +10,13 @@ function Header() {
             flex justify-between items-center p-3 
              z-50" >
 
-                <h1 className='topic text-xl cursor-pointer' onClick={() => {window.location.href='/'}}>Jonas</h1>
+                <Link href={"/"}  className="bg-white/60 sm:bg-zinc-200/60 dark:bg-zinc-900/60 backdrop-blur-[.9px] rounded-2xl p-2.5 flex justify-center items-center">
+                    <h1 className='topic text-xl cursor-pointer h-6 flex justify-center items-center'>Jonas</h1>
+                </Link>
 
                 <Options />
 
-
-            </ div>
+            </div>
             <div className='w-full h-[4.28rem]' />
         </>
     )
