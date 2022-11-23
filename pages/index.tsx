c60666b1d3134926aebe225662725bdfa79048bd
Jonas <90411737+jonas-be@users.noreply.github.com>
2022-11-23 22:29:18 +0100

    Refactor background grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,107 +5,19 @@ import {MouseParallaxChild, MouseParallaxContainer} from "react-parallax-mouse";
 import React from "react";
 import {AnimationOnScroll} from "react-animation-on-scroll";
 import "animate.css/animate.min.css";
+import BackgroundGrid from "../src/components/BackgroundGrid";
 
 function Index() {
 
 
     return (
         <div className="w-full h-full">
-            <div className="absolute overflow-hidden"
-                 style={{
-                     WebkitMaskImage: "-webkit-gradient(linear, left top, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)))",
-                     maskImage: "linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0))"
-                 } as React.CSSProperties}
-            >
-                <div className="flex flex-col items-center w-screen h-full text-violet-500">
-                    <div className="flex">
-                        <svg className="w-[850px] md:w-[1250px]" xmlns="http://www.w3.org/2000/svg"
-                             viewBox="0 0 4000 2000" fill="currentColor">
-                            <g id="layer1">
-                                <path stroke="currentColor" strokeWidth={2} d="M 0,0 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 0,500 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 0,1000 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 0,1500 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 0,2000 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="M 0,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 500,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 1000,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 1500,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 2000,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 2500,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 3000,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 3500,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 4000,0 0,2000"/>
-                            </g>
-                        </svg>
-                        <svg className="w-[850px] md:w-[1250px]" xmlns="http://www.w3.org/2000/svg"
-                             viewBox="0 0 4000 2000" fill="currentColor">
-                            <g id="layer1">
-                                <path stroke="currentColor" strokeWidth={2} d="M 0,0 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 0,500 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 0,1000 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 0,1500 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 0,2000 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="M 0,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 500,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 1000,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 1500,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 2000,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 2500,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 3000,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 3500,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 4000,0 0,2000"/>
-                            </g>
-                        </svg>
-                    </div>
-                    <div className="flex md:hidden">
-                        <svg className="w-[850px] md:w-[1250px]" xmlns="http://www.w3.org/2000/svg"
-                             viewBox="0 0 4000 2000" fill="currentColor">
-                            <g id="layer1">
-                                <path stroke="currentColor" strokeWidth={2} d="M 0,0 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 0,500 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 0,1000 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 0,1500 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 0,2000 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="M 0,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 500,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 1000,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 1500,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 2000,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 2500,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 3000,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 3500,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 4000,0 0,2000"/>
-                            </g>
-                        </svg>
-                        <svg className="w-[850px] md:w-[1250px]" xmlns="http://www.w3.org/2000/svg"
-                             viewBox="0 0 4000 2000" fill="currentColor">
-                            <g id="layer1">
-                                <path stroke="currentColor" strokeWidth={2} d="M 0,0 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 0,500 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 0,1000 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 0,1500 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 0,2000 4000,0"/>
-                                <path stroke="currentColor" strokeWidth={2} d="M 0,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 500,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 1000,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 1500,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 2000,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 2500,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 3000,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 3500,0 0,2000"/>
-                                <path stroke="currentColor" strokeWidth={2} d="m 4000,0 0,2000"/>
-                            </g>
-                        </svg>
-                    </div>
-                </div>
-            </div>
 
-            <div id="test" className="w-screen h-full" style={{backgroundImage: "/assets/grid.svg"}}>
+            <BackgroundGrid/>
 
+            <div className="w-screen h-full">
 
                 <Header/>
-
 
                 <MouseParallaxContainer
                     className="w-full h-[60vh] flex flex-col justify-center align-center"
