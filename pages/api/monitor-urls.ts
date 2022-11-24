7acad4114e4afb7c300b4c5a8f9b0ecd4a3c6a37
jobender <jbender@united-internet.de>
2022-11-24 11:07:12 +0100

    Implement monitor urls endpoint

---
+++ pages/api/monitor-urls.ts
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type {NextApiRequest, NextApiResponse} from 'next'
+
+type Data = {
+    monitorUrls: string[]
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    const response = await fetch("https://monitor.jonasbe.de/")
+    const data: string = await response.text()
+
+    const expression: string = 'https:\/\/monitor.jonasbe.de\/r\/.*\'';
+
+    let monitorUrls: string[] = []
+    // @ts-ignore
+    const regexResult = [...data.matchAll(expression)]
+    regexResult.forEach(r => {
+        monitorUrls.push(r[0].slice(0,-1))
+    })
+
+    res.status(200).json({monitorUrls: monitorUrls})
+}
