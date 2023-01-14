e1c29a065dac2a80d56af14ddde5e2303defd3f4
Jonas <90411737+jonas-be@users.noreply.github.com>
2023-01-14 16:53:17 +0100

    Implement Header spacer prop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,25 +2,33 @@ import React from 'react'
 import Options from './Options'
 import Link from "next/link";
 
-function Header() {
+function Header({spacer}: Props) {
     return (
         <>
             <div className="
             fixed w-full 
             flex justify-between items-center p-3 
-             z-50" >
+             z-50">
 
-                <Link href={"/"}  className="btn bg-accent-2 shadow-md rounded-2xl">
+                <Link href={"/"} className="btn bg-accent-2 shadow-md rounded-2xl">
                     <p className='text-accent font-bold text-lg'>Jonas</p>
                 </Link>
 
-                <Options />
+                <Options/>
 
             </div>
-            <div className='w-full h-[4.28rem]' />
+            {spacer === undefined || spacer ?
+                <div className='w-full h-[4.28rem]'/>
+                :
+                ""
+            }
         </>
     )
 }
+
+type Props = {
+    spacer?: boolean
+};
 
 
 export default Header
