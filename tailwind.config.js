fab5f465dea0fa552c794f52e7591a013f480a62
jonasbe5 <jonasbender200@gmail.com>
2023-01-25 14:27:38 +0100

    Sourced out colors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,json}",
   ],
   darkMode: 'class',
+  mode: "jit",
   theme: {
     extend: {
       borderRadius: {
@@ -21,6 +22,30 @@ module.exports = {
         'success': 'rgb(34 197 94)', //green-500
         'warning': 'rgb(251 146 60)', //orange-400
         'error': 'rgb(239 68 68)', //red-500
+        'base': {
+          0: {
+            DEFAULT: '#ffffff', // white
+            dark: '#000000', // black
+          },
+          100: {
+            DEFAULT: '#f4f4f5', // zinc-100
+            dark: '#27272a', // zinc-800
+          },
+          200: {
+            DEFAULT: '#e4e4e7', // zinc-200
+            dark: '#18181b', // zinc-900
+          },
+        },
+        'accent': {
+          100: {
+            DEFAULT: '#52525b', // zinc-600
+            dark: '#a1a1aa', // zinc-400
+          },
+          200: {
+            DEFAULT: '#000000', // black
+            dark: '#ffffff', // white
+          },
+        }
       },
       animation: {
         'ping-in': 'ping 1s cubic-bezier(0, 0, 0.2, 1) 1 reverse',
