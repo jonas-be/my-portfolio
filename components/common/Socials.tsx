ecdb7ea3bdd84d762e384ded498b57808e880b4d
jonas-be <jonasbender2005@gmail.com>
2023-03-29 08:55:46 +0200

    Update socials color

--- components/common/Socials.tsx
+++ components/common/Socials.tsx
@@ -13,11 +13,11 @@ const Socials = ({socials, big}: Props) => {
 
     return (
         <div className="flex justify-center overflow-x-hidden">
-            <Link className={`btn mx-2 text-xl ${big ? "text-2xl rounded-2xl md:mx-4":""} bg-[#1DA1F2] text-accent`} href={socials.twitter} title={socials.twitterTitle}><FaTwitter/></Link>
-            <Link className={`btn mx-2 text-xl ${big ? "text-2xl rounded-2xl md:mx-4":""} bg-[#333] text-accent`} href={socials.github} title={socials.githubTitle}><FaGithub/></Link>
-            <Link className={`btn mx-2 text-xl ${big ? "text-2xl rounded-2xl md:mx-4":""} bg-[#5865F2] text-accent`} href={socials.discordServer} title={socials.discordServerTitle}><FaUserFriends/></Link>
-            <Link className={`btn mx-2 text-xl ${big ? "text-2xl rounded-2xl md:mx-4":""} bg-[#5865F2] text-accent`} href={socials.discordUser} title={socials.discordUserTitle}><FaDiscord/></Link>
-            <Link className={`btn mx-2 text-xl ${big ? "text-2xl rounded-2xl md:mx-4":""} bg-[#0db7ed] text-accent`} href={socials.docker} title={socials.dockerTitle}><FaDocker/></Link>
+            <Link className={`btn mx-2 text-xl ${big ? "text-2xl rounded-2xl md:mx-4":""} bg-[#1DA1F2] text-accent-dark`} href={socials.twitter} title={socials.twitterTitle}><FaTwitter/></Link>
+            <Link className={`btn mx-2 text-xl ${big ? "text-2xl rounded-2xl md:mx-4":""} bg-[#5865F2] text-accent-dark`} href={socials.discordServer} title={socials.discordServerTitle}><FaUserFriends/></Link>
+            <Link className={`btn mx-2 text-xl ${big ? "text-2xl rounded-2xl md:mx-4":""} bg-[#333] text-accent-dark`} href={socials.github} title={socials.githubTitle}><FaGithub/></Link>
+            <Link className={`btn mx-2 text-xl ${big ? "text-2xl rounded-2xl md:mx-4":""} bg-[#5865F2] text-accent-dark`} href={socials.discordUser} title={socials.discordUserTitle}><FaDiscord/></Link>
+            <Link className={`btn mx-2 text-xl ${big ? "text-2xl rounded-2xl md:mx-4":""} bg-[#0db7ed] text-accent-dark`} href={socials.docker} title={socials.dockerTitle}><FaDocker/></Link>
         </div>
 
     );
